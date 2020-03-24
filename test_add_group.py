ce8240f1034bf3c660043300375bd7a9d67824bd
Nichita <pzvn95@gmail.com>
2020-03-24 16:47:22 +0300

    Модификация кода и момент рефакторинга

--- test_add_group.py
+++ test_add_group.py
@@ -4,8 +4,9 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from group import Group
 
-class TestAddGroup(unittest.TestCase):
+class test_add_group(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
@@ -14,41 +15,50 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         self.Open_home_page(wd)
-
-        self.Login(wd)
-        self.Open_and_Create_Group(wd)
+        self.Login(wd, username="admin", password="secret")
+        self.Open_and_Create_Group(wd, Group(name="qwerty", header="qwertyes", footer="qwertyez"))
         #return to groups page
         wd.find_element_by_link_text("group page").click()
         #logout
         wd.find_element_by_link_text("Logout").click()
 
-    def Open_and_Create_Group(self, wd):
-        # open groups page
-        wd.find_element_by_link_text("groups").click()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group firm
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("qwerty")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("qwerty")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("qwerty")
-        # submit
-        wd.find_element_by_name("submit").click()
+    def add_empty_group(self):
+            wd = self.wd
+            self.Open_home_page(wd)
+            self.Login(wd, username="admin", password="secret")
+            self.Open_and_Create_Group(wd, Group(name="", header="", footer=""))
 
-    def Login(self, wd):
+            wd.find_element_by_link_text("group page").click()
+
+            wd.find_element_by_link_text("Logout").click()
+
+    def Login(self, wd, username, password):
         # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def Open_and_Create_Group(self, wd, group):
+         # open groups page
+            wd.find_element_by_link_text("groups").click()
+        # init group creation
+            wd.find_element_by_name("new").click()
+            # fill group firm
+            wd.find_element_by_name("group_name").click()
+            wd.find_element_by_name("group_name").clear()
+            wd.find_element_by_name("group_name").send_keys(group.name)
+            wd.find_element_by_name("group_header").click()
+            wd.find_element_by_name("group_header").clear()
+            wd.find_element_by_name("group_header").send_keys(group.header)
+            wd.find_element_by_name("group_footer").click()
+            wd.find_element_by_name("group_footer").clear()
+            wd.find_element_by_name("group_footer").send_keys(group.footer)
+            # submit
+            wd.find_element_by_name("submit").click()
 
     def Open_home_page(self, wd):
         # open home page
@@ -63,8 +73,6 @@ class TestAddGroup(unittest.TestCase):
         try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
-    
-
     
     def tearDown(self):
         self.wd.quit()
